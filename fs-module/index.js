896a62a70ccad2c08ebac285540846dc099c90c0
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-24 13:48:34 +0530

    push all fs-module functionality with fixing return

--- fs-module/index.js
+++ fs-module/index.js
@@ -3,78 +3,80 @@ const fs = require('fs');
 
 //Read File
 
-// // Asynchronous Reading
-// fs.readFile('vikram.txt', 'utf-8', (err, data) => {
-//     if (err) {
-//         console.error("Error reading file :", err);
-//         return;
-//     }
-//     console.log("File contents :\n", data);
+// Asynchronous Reading
+fs.readFile('vikram.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.error("Error reading file :", err);
+        return;
+    }
+    console.log("File contents :\n", data);
     
-// })
+})
 
-// // Synchronous Reading
-// try {
-//     const data = fs.readFileSync('vikram.txt', 'utf-8');
-//     console.log("File contents :\n", data);
+// Synchronous Reading
+try {
+    const data = fs.readFileSync('vikram.txt', 'utf-8');
+    console.log("File contents :\n", data);
     
-// } catch (error) {
-//     console.error("Error reading file :", err);
+} catch (error) {
+    console.error("Error reading file :", err);
     
-// }
+}
 
 // Asynchronous Writing File
-// fs.writeFile('output.txt', 'Some temp text', (err) => {
-//     if (err) {
-//         console.error('Error writing file:', err);
-//     }
-//     console.log("File written successfully.");
-// })
+fs.writeFile('output.txt', 'Some temp text', (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+        return;
+    }
+    console.log("File written successfully.");
+})
 
 
 // synchronous Writing File
-// try {
-//     fs.writeFileSync('syncOutput.txt', 'Hello!');
-//     console.log('File written successfully.');
+try {
+    fs.writeFileSync('syncOutput.txt', 'Hello!');
+    console.log('File written successfully.');
     
-// } catch (error) {
-//     console.error('Error writing file:', error);
+} catch (error) {
+    console.error('Error writing file:', error);
     
-// }
+}
 
 // Appending to Files
 // Asynchronous Appending
 
-// fs.appendFile('output.txt', "\n Added something", (err) => {
-//     if (err) {
-//         console.error("Error :", err);
+fs.appendFile('output.txt', "\n Added something", (err) => {
+    if (err) {
+        console.error("Error :", err);
+        return;
         
-//     }
-//     console.log("content added successfully.");
+    }
+    console.log("content added successfully.");
     
-// })
+})
 
 // synchronous Appending
-// try {
-//     fs.appendFileSync('output.txt', '..added something new')
-//     console.log("content added sucessfully.");
+try {
+    fs.appendFileSync('output.txt', '..added something new')
+    console.log("content added sucessfully.");
     
-// } catch (error) {
-//     console.error(error);
+} catch (error) {
+    console.error(error);
     
-// }
+}
 
 // Deleting Files
 // Asynchronous Deletion
-// fs.unlink('output.txt', (err) => {
-//     if (err) {
-//         console.error("ERROR in deleting", err);
-//         return;
+fs.unlink('output.txt', (err) => {
+    if (err) {
+        console.error("ERROR in deleting", err);
+        return;
         
-//     }
-//     console.log("file deleted successfully");
+    }
+    console.log("file deleted successfully");
     
-// })
+})
 
 // synchronous Deletion
 try {
