62921caff9a1e45fb34e889f8d8a37b45199596c
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-24 12:48:34 +0530

    read file Asynchronously

---
+++ fs-module/index.js
@@ -0,0 +1,16 @@
+const { log } = require('console');
+const fs = require('fs');
+
+//Read File
+
+//Asynchronous Reading
+fs.readFile('vikram.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.error("Error reading file :", err);
+        return;
+    }
+    console.log("File contents :\n", data);
+    
+})
+
+
