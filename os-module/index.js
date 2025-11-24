fcd4a0ed02a2d4de4914fc7ae4f9f4f3e479b787
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-24 18:15:09 +0530

    added os-module

---
+++ os-module/index.js
@@ -0,0 +1,17 @@
+const os = require('os');
+
+console.log("CPU Architecture :", os.arch());
+console.log("Free memeory :", os.freemem());
+console.log("Total memory :", os.totalmem());
+console.log("Network interfaces :", os.networkInterfaces());
+console.log("OS default temperary directory :", os.tmpdir());
+console.log("Endianness :", os.endianness());
+console.log("Hostname :", os.hostname());
+console.log("OS type :", os.type());
+console.log("OS platform :", os.platform());
+console.log("OS release :", os.release());
+
+
+
+
+
