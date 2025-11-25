7e833e82989a2d057930315ac452d42d450751bd
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-25 19:20:50 +0530

    learned statusCode and setHeader

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,12 @@
-const http = require('http')
+const http = require('http');
+const { json } = require('stream/consumers');
 
 const PORT = 8000
 
 const server = http.createServer((req, res) => {
-    res.end('Node server created by Vikram!');
+    res.statusCode= 200;
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({error:'Server Error!'}));
 })
 
 server.listen(PORT, () => {
